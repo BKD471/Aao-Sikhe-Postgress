e01394a47886b751e7990f72f9f39accb1828c82
BKD471 <bhaskarkumardas9@gmail.com>
2023-04-29 23:02:57 +0530

    d4

---
+++ Day4.sql
@@ -0,0 +1,37 @@
+---- delete constraint 
+--------------------------------------------------------- on del cascade
+
+CREATE TABLE photos (
+id SERIAL PRIMARY KEY,
+url VARCHAR(200),
+user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+);
+ 
+INSERT INTO photos (url, user_id)
+VALUES
+('http:/one.jpg', 4),
+('http:/two.jpg', 1),
+('http:/25.jpg', 1),
+('http:/36.jpg', 1),
+('http:/754.jpg', 2),
+('http:/35.jpg', 3),
+('http:/256.jpg', 4);
+
+SELECT * FROM USERS WHERE id=1;
+
+------------------------------------------------------------- on set null constraint
+CREATE TABLE photos (
+id SERIAL PRIMARY KEY,
+url VARCHAR(200),
+user_id INTEGER REFERENCES users(id) ON SET NULL
+);
+ 
+INSERT INTO photos (url, user_id)
+VALUES
+('http:/one.jpg', 4),
+('http:/two.jpg', 1),
+('http:/25.jpg', 1),
+('http:/36.jpg', 1),
+('http:/754.jpg', 2),
+('http:/35.jpg', 3),
+('http:/256.jpg', 4);
